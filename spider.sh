24b745c048512a183b9cfa25355c88f7b7547261
Entwickler <marius.reineck@msg.group>
2018-10-17 08:11:22 +0200

    Spider verbessert

--- spider.sh
+++ spider.sh
@@ -3,12 +3,26 @@
 export http_proxy=""
 export https_proxy=""
 
-seiten=("www.berlin.de" "https://www.hamburg.de/buergerservice/" "www.gelsenkirchen.de/de/_meta/Buergerservice/" "www.koeln.de")
+# Alle zu synchronisierenden Seiten
+seiten=("www.berlin.de" "https://www.hamburg.de/buergerservice/" "www.gelsenkirchen.de/de/_meta/Buergerservice/" "www.koeln.de" "www.frankfurt.de/sixcms/detail.php?id=2717")
 
-cd /home/developer/staedte/data
+# Ort, an welchen die Seiten geschrieben werden sollen
+ziel=/home/developer/staedte/data
 
+# Wechsel zum Zielverzeichnis
+cd $ziel
+
+# Für jede Seite im hintergrund einen wget crawl Prozess starten
 for element in "${seiten[@]}"
 do
-    wget -A.htm.html.aspx -r -p -U -N "$element" &
+    wget -A.htm,.html,.aspx,.php -o /dev/null -r -p -U -N "$element" &
 done 
 
+# Jede Sekunde verbrauchten Speicherplatz anzeigen
+watch -n1 du -sh $ziel
+
+# Falls abbruch durch CTRL-C oder ähnliches alle wget Prozesse stoppen
+function finish {
+  killall -9 wget
+}
+trap finish EXIT
