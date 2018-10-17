3a5e679de957a054271e3e68a346320df0cbdaeb
Entwickler <marius.reineck@msg.group>
2018-10-17 12:36:41 +0200

    wordcount by file

--- wordcount.py
+++ wordcount.py
@@ -1,29 +1,37 @@
 from glob import glob
 import os
 import re
+import sys
 
 maxLen = 30
 minLen = 4
 files = []
-start_dir = "/home/developer/staedte/data/"
-pattern   = "*.aspx"
+# For Testing no boundary/ argchecks
+start_dir = sys.argv[1]
+out_file = sys.argv[2]
+
+pattern   = "*.*"
 
 for dir,_,_ in os.walk(start_dir):
     files.extend(glob(os.path.join(dir,pattern))) 
 
 wordcount = {}
+out = open(out_file, "w")
 for f in files:
+	if not os.path.isfile(f):
+		continue
+	filePathClean = f[len(start_dir):]
 	file=open(f)
-	wordcount[f] = {}
+	wordcount[filePathClean] = {}
 	for word in re.findall(r"[\w']+", file.read()):
 		if len(word) >= minLen and len(word) <= maxLen:
-			if word not in wordcount[f]:
-			    wordcount[f][word] = 1
+			if word not in wordcount[filePathClean]:
+			    wordcount[filePathClean][word] = 1
 			else:
-			    wordcount[f][word] += 1
-
+			    wordcount[filePathClean][word] += 1
+	for f,w in wordcount.items():
+		for w,n in wordcount[f].items():
+			out.write(f.split("/")[0]+","+"/".join(f.split("/")[1:])+","+w+","+str(n)+"\n")
+	wordcount = {}
 	file.close();
-for f,w in wordcount.items():
-	for w,n in wordcount[f].items():
-		print f,w,n
-
+out.close()
